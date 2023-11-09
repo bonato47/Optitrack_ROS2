ecad8c3a466c30fcd42193dae32f293028389112
bonato47 <tristan_bonato@hotmail.com>
2023-11-09 15:14:59 +0100

    clean folder
    clean folder

---
+++ ros2_ws/install/ros1_bridge/share/ros1_bridge/cmake/find_ros1_package.cmake
@@ -0,0 +1,1 @@
+/home/bonato/Documents/Ros2_ws/src/ros1_bridge/cmake/find_ros1_package.cmake
--- src/optitrack/docker-run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-docker run \
-	   -it \
-	   -e DISPLAY=$DISPLAY \
-	   -h $HOSTNAME \
-	   -u root \
-	   --net host \
-	   optitrack_zmq
