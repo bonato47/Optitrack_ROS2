0d766af2ec09c834c965cdd21663bc673f7e99b8
bonato47 <tristan_bonato@hotmail.com>
2023-11-16 16:37:21 +0100

    s

--- ros1_ws/src/optitrack_publisher/src/optitrack_transform_publisher.cpp
+++ ros1_ws/src/optitrack_publisher/src/optitrack_transform_publisher.cpp
@@ -75,8 +75,8 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "objectbase");
     ros::NodeHandle Nh;
 
-    Nh.getParam("/vrpn_client_node/name_object", name_object);
-    Nh.getParam("/vrpn_client_node/name_base", name_base);
+    Nh.getParam("/optitrack_publisher/name_object", name_object);
+    Nh.getParam("/optitrack_publisher/name_base", name_base);
 
 
     string name_object_transform = name_object+ "_transform";
